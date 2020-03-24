4e3184e1ae390bebc2ce5cbdf92f0b3ac0d5cddd
JCesar <jcesarfb7@gmail.com>
2020-03-23 23:49:56 -0700

    quedo el diseño de la home

---
+++ audipaq/resources/views/layouts/footer.blade.php
@@ -0,0 +1,46 @@
+<div class="container-fluid" style="background-color: #546E7A"	 >
+</div>
+
+<div class="container-fluid" style="background-color: #546E7A"	>
+	<hr style="border-color: #546E7A; width: 100%; border-width: 3px">
+   <div class="row">
+	  <div class="col-md-4">
+	  	<p align="center" style="color: #fff">
+	      <b style="color: #fff">Contacto</b>
+	    </p>
+	    <p align="center" style="color: #fff">
+	  	ejemplo@tudominio.com
+	    </p>
+        <p align="center" style="color: #fff">
+	  	Tel: 324-102-3790<br>
+	  	Tel: 324-105-6850
+	    </p>
+	    
+	  </div>
+	  <div class="col-md-4">
+	  	<p align="center">
+	      <b style="color: #fff">Redes sociales </b>
+	    </p>
+	      <p align="center">
+	  	<img src="images/iconoFacebook.png" width="30" height="30"	class="d-inline-block align-top">
+	  	<img src="images/iconoWhatsapp.png" width="30" height="30"	class="d-inline-block align-top">
+	  
+	    </p>
+	  </div>
+  </div>
+   <p align="center">
+  <b style="color: #fff"> AUDIPAQ INC. 2020. IXTLÁN DEL RÍO, NAYARIT, MÉXICO</b>
+  </p>
+</div>
+
+</body>
+<footer>
+<script type="text/javascript" src="js/Validaciones.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="js/form.js"></script>
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	 <script type="text/javascript" src="js/main.js"></script>
+</footer>
+</html>
---
+++ audipaq/resources/views/layouts/menu_Navegacion.blade.php
@@ -0,0 +1,37 @@
+{{-------------------------Menú de navegación---------------------------}}
+<div class="container-fluid" style="background-color: #546E7A"> 
+<div class="row"> 
+<div class="col-md-12">
+<nav class="navbar navbar-expand-lg	navbar-light bg-light" style="background-color: #546E7A !important">
+    <img src="images/audipaq.jpg" width="200" height="60"	class="d-inline-block align-top" alt="">
+
+	<button	class="navbar-toggler"	type="button"	data-toggle="collapse"data-target="#navbarSupportedContent"	aria-controls="navbarSupportedContent" aria-expanded="false"	aria-label="Toggle	navigation">
+	<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul	class="navbar-nav ml-auto">
+			<a href="{{ url('/')}}">
+			<a	class="navbar-brand" href="#" style="color: #fff"><b>Inicio</b></a>
+            </a>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<li class="nav-item dropdown">
+			<a lass="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<a	class="navbar-brand" href="#" style="color: #fff"><b>Iniciar sesión</b></a>
+            </a>
+            </li>
+		</ul>
+	</div>
+</nav>
+</div>
+</div>
+
+
+
+	      
+        </div>
+    </div>
+</div>
+
+
+</div>
+{{-----------------------FinMenú de navegación---------------------------}}
